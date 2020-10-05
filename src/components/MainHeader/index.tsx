606b562db3c4a4f276c760bc4cd6fcc6e1879e20
Eduardo Amâncio <eduardo.amncio@gmail.com>
2020-10-04 22:56:23 -0300

    Criado o header da pagina, com o toggle para mudar o tema(sem funcionalidade ainda)

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
-import { Container } from './styles';
+import React, {useMemo} from 'react';
+import { 
+  Container, 
+  Profile, 
+  Welcome, 
+  UserName } from './styles';
+
+import {arEmojis} from '../../utils/emojis';
+import Toggle from '../Toggle';
 
 const MainHeader: React.FC = () => {
+
+  const emoji = useMemo(() =>{
+    const indice = Math.floor(Math.random() * arEmojis.length);
+    return arEmojis[indice];
+  }, []);
+
   return (
     <>
       <Container>
-        <h1>MainHeader page</h1>
+        <Toggle></Toggle>
+        <Profile>
+  <Welcome>Olá, {emoji}</Welcome>
+          <UserName>Eduardo Amâncio</UserName>
+        </Profile>
       </Container>
     </>
   );
---
+++ src/components/Toggle/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Container, ToggleLabel, ToggleSelector} from './styles';
+
+
+const Toggle: React.FC = () => {
+  return (
+    <Container>
+      <ToggleLabel>Light</ToggleLabel>
+      <ToggleSelector 
+        checked = {true}
+        uncheckedIcon = {false}
+        checkedIcon = {false}
+        onChange= {() => console.log("change")}
+
+      />
+      <ToggleLabel>Dark</ToggleLabel>
+    </Container>
+  );
+};
+
+export default Toggle;
